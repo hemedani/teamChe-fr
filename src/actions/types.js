7ef445464f6bfc88faea7505c17bb3972dfe2860
Syd Amir <hemedani@gmail.com>
2019-04-22 01:00:06 +0430

    change api constance

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,9 +1,9 @@
 /* Address localhost */
-// export const RU = "/api";
+export const RU = "/api";
 // export const RU = "http://198.143.182.169:1367/api";
 
 // Address VPS Khodam (AzarOnline)
-export const RU = "http://37.120.146.71:1367/api";
+// export const RU = "http://37.120.146.71:1367/api";
 
 export const API_KEY = "AIzaSyDHrAOj672WWAVKh9bHho2Cbh5obIaNgOE";
 export const API_KEY2 = "AIzaSyC4A2YR8nqDSc3anI4kuPutQBf49Jak1fw";
